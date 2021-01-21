0c9c224cb6577c6ea8773ad4be3c7d8e52282d9d
Vitor <vitorhugobrizante@gmail.com>
2021-01-21 00:10:10 -0300

    Projeto gerando senha e salvando

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 import PySimpleGUI as sg
-import tkinter
 import os
 
 
@@ -10,11 +9,11 @@ class PassGen:
         # Layout
         sg.theme('Black')
         layout = [
-            [sg.Text('Site/Software', size=(10, 1)),
+            [sg.Text('Site/Software', size=(12, 1)),
              sg.Input(key='site', size=(20, 1))],
-            [sg.Text('E-mail/Usuario', size=(10, 1)),
+            [sg.Text('E-mail/Usuario', size=(12, 1)),
              sg.Input(key='usuario', size=(20, 1))],
-            [sg.Text('Quantidade de caracteres'), sg.Combo(values=(
+            [sg.Text('Quantidade de caracteres'), sg.Combo(values=list(
                 range(30)), key='total_chars', default_value=1, size=(3, 1))],
             [sg.Output(size=(35, 5))],
             [sg.Button('Gerar Senha')]
@@ -28,10 +27,22 @@ class PassGen:
             evento, valores = self.janela.read()
             if evento == sg.WINDOW_CLOSED:
                 break
-        pass
+            if evento == 'Gerar Senha':
+                nova_senha = self.gerar_senha(valores)
+                print(nova_senha)
+                self.salvar_senha(nova_senha, valores)
+    def gerar_senha(selfself, valores):
+        char_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%*'
+        chars = random.choices(char_list,k=int(valores['total_chars']))
+        new_pass = ''.join(chars)
+        return new_pass
 
-    def salvar_senha(self):
-        pass
+    def salvar_senha(self, nova_senha, valores):
+        with open('senhas.txt', 'a', newline='') as arquivo:
+            arquivo.write(
+                f"site: {valores['site']}, usuario: {valores['usuario']}, nova senha: {nova_senha}")
+            print('Arquivo salvo')
+
 
 
 gen = PassGen()
